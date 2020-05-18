00c25bbe29859b820fcce23d7cb5ba56a241838d
Kyubaek-Kim <48429878+Kyubaek-Kim@users.noreply.github.com>
2020-05-18 19:31:13 +0900

    Add files via upload

---
+++ 6장 학습관련 기술들.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+
+class SGD:
+    def __init__(self, lr = 0.01):
+        self.lr = lr
+    
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key]
+            
+    
+
+
+# In[ ]:
+
+
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+    
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)# 인풋과 같은 shape의 0행렬 반환
+            
+        for key in paramms.keys():
+            self.v[key] = self.momentum*self.v[key] - self.lr*grads[key]
+            params[key] += self.v[key]
+
+
+# In[7]:
+
+
+class AdaGrad:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
+        
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+        
+
+
+# In[9]:
+
+
+# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def ReLU(x):
+    return np.maximum(0, x)
+
+
+def tanh(x):
+    return np.tanh(x)
+    
+input_data = np.random.randn(1000, 100)  # 1000개의 데이터
+node_num = 100  # 각 은닉층의 노드(뉴런) 수
+hidden_layer_size = 5  # 은닉층이 5개
+activations = {}  # 이곳에 활성화 결과를 저장
+
+x = input_data
+
+for i in range(hidden_layer_size):
+    if i != 0:
+        x = activations[i-1]
+
+    # 초깃값을 다양하게 바꿔가며 실험해보자！
+    w = np.random.randn(node_num, node_num) * 1
+    # w = np.random.randn(node_num, node_num) * 0.01
+    # w = np.random.randn(node_num, node_num) * np.sqrt(1.0 / node_num)
+    # w = np.random.randn(node_num, node_num) * np.sqrt(2.0 / node_num)
+
+
+    a = np.dot(x, w)
+
+
+    # 활성화 함수도 바꿔가며 실험해보자！
+    z = sigmoid(a)
+    # z = ReLU(a)
+    # z = tanh(a)
+
+    activations[i] = z
+
+# 히스토그램 그리기
+for i, a in activations.items():
+    plt.subplot(1, len(activations), i+1)
+    plt.title(str(i+1) + "-layer")
+    if i != 0: plt.yticks([], [])
+    # plt.xlim(0.1, 1)
+    # plt.ylim(0, 7000)
+    plt.hist(a.flatten(), 30, range=(0,1))
+plt.show()
+
+
+# In[10]:
+
+
+class Dropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+    
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+        
+    def backward(self, dout):
+        return dout * self.mask
+
+
+# In[11]:
+
+
+weight_decay = 10 ** np.random.uniform(-8, -4)
+lr = 10 ** np.random.uniform(-6, -2)
+
+
+# In[ ]:
+
+
+
+
